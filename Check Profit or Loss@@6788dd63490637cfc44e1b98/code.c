1557494060d5ba7e54a9c0b658ce31941a6448ac
Ashmit1466 <195905560+Ashmit1466@users.noreply.github.com>
2025-02-10 16:21:37 +0530

    Solution to Check Profit or Loss

--- Check Profit or Loss@@6788dd63490637cfc44e1b98/code.c
+++ Check Profit or Loss@@6788dd63490637cfc44e1b98/code.c
@@ -1,24 +1,16 @@
 #include <stdio.h>
-int main (){
-    int grade;
-    scanf("%d", &grade);
-
-    if (grade >= 90){
-        printf("A");
+int main(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if ( a < b){
+        printf("Profit");
     }
-    else if (grade >= 80 && grade < 90){
-        printf("B");
-    }
-    else if (grade >= 70 && grade < 80){
-        printf("C");
-    }
-    else if (grade >= 60 && grade < 70){
-        printf("D");
-    }
-    else if (grade < 60){
-        printf("F");
+    else if (a > b){
+        printf("Loss");
     }
     else{
-        printf("Enter a valid number");
+        printf("No Profit No Loss");
     }
-    return 0;}
+    return 0;
+
+}
