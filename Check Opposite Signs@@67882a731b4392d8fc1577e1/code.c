9e55e8ad8fd7b1888f41122d602373dbff9bb432
Ashmit Gautam <195905560+Ashmit1466@users.noreply.github.com>
2025-02-18 02:21:38 +0530

    Solution to Check Opposite Signs

--- Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
+++ Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main(){
-    int num1, num2;
-    scanf("%d %d", &num1, &num2);
-    if(num1 > 0 && num2 < 0 || num1 < 0 && num2 > 0){
-        printf("True");
+    int a,b;
+    scanf("%d %d",&a,&b);
+
+    if (a*b<0){
+    printf("True");
     }
     else{
         printf("False");
