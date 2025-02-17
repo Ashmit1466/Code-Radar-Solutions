5a59d036ec4a78b05fbba8dc19fef3ba7a27acd4
Ashmit Gautam <195905560+Ashmit1466@users.noreply.github.com>
2025-02-18 02:41:53 +0530

    Solution to Perform Bitwise XOR

--- Perform Bitwise XOR@@67882a731b4392d8fc1577e4/code.c
+++ Perform Bitwise XOR@@67882a731b4392d8fc1577e4/code.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 int main(){
-    int num1, num2;
-    int xor;
-    scanf("%d %d", &num1, &num2);
-    xor = num1 ^ num2;
-    printf("%d", xor);
-    return 0;
+   int a,b;
+   scanf("%d %d",&a,&b);
+   printf("%d",a^b);
+   return 0;
 }
